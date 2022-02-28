0152b64734b6d5e7fa8a7771a304c52717c0bde2
Dawa Sherpa <Dawa.Sherpa@hendry.com.au>
2022-02-28 15:52:08 +1100

    add welcome name in checkout

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,21 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+// other services is needed
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyABwXCpUJ2du7wAPzz8lbmoBm-_Rknoo2A",
-    authDomain: "challenge-16780.firebaseapp.com",
-    projectId: "challenge-16780",
-    storageBucket: "challenge-16780.appspot.com",
-    messagingSenderId: "190979974900",
-    appId: "1:190979974900:web:ba3a6a79c8b946a3a9a0a1",
-    measurementId: "G-PRJLQJLL1R"
-  };
+  apiKey: "AIzaSyABwXCpUJ2du7wAPzz8lbmoBm-_Rknoo2A",
+  authDomain: "challenge-16780.firebaseapp.com",
+  projectId: "challenge-16780",
+  storageBucket: "challenge-16780.appspot.com",
+  messagingSenderId: "190979974900",
+  appId: "1:190979974900:web:ba3a6a79c8b946a3a9a0a1",
+  measurementId: "G-PRJLQJLL1R"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
